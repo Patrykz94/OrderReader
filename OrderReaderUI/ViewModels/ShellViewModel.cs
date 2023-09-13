0555ef35f871c2a6092a32d5ea4c0c7c6154dea3
Patryk Zawierucha <zawpat@googlemail.com>
2023-09-13 18:02:33 +0100

    Create a new UI Project and start working on styling controls

---
+++ OrderReaderUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace OrderReaderUI.ViewModels;
+
+public class ShellViewModel : Conductor<object>
+{
+    public string CurrentVersion { get; set; }
+
+    public ShellViewModel()
+    {
+        // Get the current version of our app
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+        CurrentVersion = $" v{versionInfo.FileVersion}";
+    }
+
+    protected async override void OnViewLoaded(object view)
+    {
+        base.OnViewLoaded(view);
+        await Orders();
+    }
+
+    public Task Orders()
+    {
+        return ActivateItemAsync(IoC.Get<OrdersViewModel>());
+    }
+
+    public Task Customers()
+    {
+        return ActivateItemAsync(IoC.Get<CustomersViewModel>());
+    }
+
+    public Task Settings()
+    {
+        return ActivateItemAsync(IoC.Get<SettingsViewModel>());
+    }
+
+    public void ExitApplication()
+    {
+        TryCloseAsync();
+    }
+}
