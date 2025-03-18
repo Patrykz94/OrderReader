91a2a0015b0c4ef9066d1ea740c7612010742561
Patryk <zawpat@googlemail.com>
2025-03-18 12:38:29 +0000

    * Fix scrolling issue when hovering over a DataGrid by manually handling the event.

--- OrderReader/Controls/Orders/OrderListItemView.xaml.cs
+++ OrderReader/Controls/Orders/OrderListItemView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace OrderReader.Controls.Orders;
 
@@ -7,5 +10,31 @@ public partial class OrderListItemView : UserControl
     public OrderListItemView()
     {
         InitializeComponent();
+    }
+
+    private void OrdersView_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        var scrollViewer = FindAncestor<ScrollViewer>(sender as DependencyObject);
+        if (scrollViewer != null)
+        {
+            var scrollStep = e.Delta / SystemParameters.WheelScrollLines;
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - scrollStep);
+            e.Handled = true; // Prevents the DataGrid from handling the event
+        }
+    }
+
+    private static T? FindAncestor<T>(DependencyObject? child) where T : DependencyObject
+    {
+        if (child is null) return null;
+        var parent = VisualTreeHelper.GetParent(child);
+        while (parent != null)
+        {
+            if (parent is T correctlyTyped)
+                return correctlyTyped;
+
+            parent = VisualTreeHelper.GetParent(parent);
+        }
+
+        return null;
     }
 }
